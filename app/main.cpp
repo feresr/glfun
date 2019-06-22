6b65b9324c9f5091f19464bd5d668ac6e1333fe1
Fernando Raviola <fernandoraviola@gmail.com>
2019-06-23 01:31:52 +1000

    Initial Commit

---
+++ app/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  app
+//
+//  Created by Fernando Raviola on 23/06/2019.
+//  Copyright © 2019 Fernando Raviola. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
